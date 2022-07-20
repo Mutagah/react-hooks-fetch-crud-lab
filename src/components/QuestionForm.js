843b4a60490b3f185701ed106affa8990f2f1609
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-20 14:56:38 +0300

    Implemented post,get,delete and updating on state

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
@@ -9,7 +9,7 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
+  
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -19,9 +19,25 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    fetch("http://localhost:4000/questions",{
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body : JSON.stringify(
+      {
+        "prompt" : formData.prompt,
+        "answers" : [formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        "correctIndex" : formData.correctIndex
+      })
+      
+    })
     console.log(formData);
   }
-
   return (
     <section>
       <h1>New Question</h1>
